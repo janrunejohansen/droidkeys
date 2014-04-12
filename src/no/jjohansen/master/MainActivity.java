7b2fc2a7941d0809762c679a23e5961d22419148
janrunejohansen@gmail.com <janrunejohansen@gmail.com>
2014-04-12 13:37:12 +0000

    Endret kommentarer i MainActivity

--- src/no/jjohansen/master/MainActivity.java
+++ src/no/jjohansen/master/MainActivity.java
@@ -1,26 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package no.jjohansen.master;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import org.library.*;
 
 /**
  *
- * @author Spartacus Rex
+ * @author Jan Rune Johansen
  */
 public class MainActivity extends Activity {
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        // ToDo add your GUI initialization code here
+        // Aktivitet starter her
         setContentView(R.layout.main);
 	
 		String test = libfunc.getMessage();
